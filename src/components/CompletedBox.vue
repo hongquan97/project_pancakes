78466a6734e53dd040f43cdfa9515df9c80dd320
vallipichappan <vallipichappan@users.noreply.github.com>
2020-02-25 19:54:35 +0300

    Opacity

--- src/components/CompletedBox.vue
+++ src/components/CompletedBox.vue
@@ -2,6 +2,7 @@
   <div>
     Completed Module Code:
     <input type="text" maxlength="8" v-model="Module"/>
+    <input type="button" value="Enter" v-on:click="CheckModule()"/>
 
     <p id="Completed">
       <h1>Completed Modules</h1>
@@ -19,18 +20,65 @@
       <div id="GE">
         <b>General Electives</b>
       </div>
+      <External :Module = "CoreModule"></External>
 
   </div>
 </template>
 
 <script>
+import External from './ExternalWebpage.vue'
+
 export default {
   data() {
     return {
       CompletedCore: [],
-      Module: ""
+      Module: "",
+      CoreModule:"",
+      PE: [],
+      CompletedPE: [],
+      CompletedGE: [],
+      CompletedUE: [],
+      UC_CM_Count: 84,
+      UC_PE_Count: 24,
+      UC_UE_Count: 32,
+      UC_GE_Count: 20
     }
+  },
+  components:{
+    External
+
+  },
+
+  methods: {
+    CheckModule: function() {
+      const searchTerm = this.Module.toUpperCase().trim();
+      if (searchTerm == "") {
+        alert("Enter something!");
+      } else {
+        if (this.Core.indexOf(searchTerm) != -1) {
+          this.CompletedCore.push(searchTerm);
+          this.UC_CM_Count-=4;
+          this.CoreModule = searchTerm;
+          // if (searchTerm == "IS4010") {
+          //   this.UC_CM_Count -= 12;
+          // } else if (searchTerm == "BT4101") {
+          //   this.UC_CM_Count -= 12;
+          // } else if (searchTerm == "BT4103") {
+          //   this.UC_CM_Count -= 8;
+          // } else {
+          //   this.UC_CM_Count -= 4;
+          // }
+        } else {
+          if (searchTerm.substring(0, 2) == "GE") {
+            this.CompletedGE.push(searchTerm);
+            this.UC_GE_Count -= 4;
+          }
+        }
+      }
+      
+      this.Module = "";
   }
+}
 }
 </script>
 
