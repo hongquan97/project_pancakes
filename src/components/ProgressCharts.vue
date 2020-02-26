1ec7f388abe4e1c2cbef924010fa83848c77ff8b
hongquan97 <61221709+hongquan97@users.noreply.github.com>
2020-02-26 14:50:34 +0800

    Removed comments in ProgressCharts.vue

--- src/components/ProgressCharts.vue
+++ src/components/ProgressCharts.vue
@@ -4,32 +4,6 @@
     <PEProgress></PEProgress>
     <UEProgress></UEProgress>
     <GEProgress></GEProgress>
-  <!-- comment 
-    <Progress strokeColor="#fbefcc" :transitionDuration="1000" :radius="70" :strokeWidth="15" value="70.00">
-      <template v-slot:footer>
-        <b>Core Module</b>
-      </template>
-    </Progress>
-    
-    <Progress strokeColor="#f9ccac" :transitionDuration="1000" :radius="70" :strokeWidth="15" value="50.00">
-      <template v-slot:footer>
-        <b>Programme Electives</b>
-      </template>
-    </Progress>
-
-    <Progress strokeColor="#f4a688" :transitionDuration="1000" :radius="70" :strokeWidth="15" value="60.00">
-      <template v-slot:footer>
-        <b>Unrestricted Electives</b>
-      </template>
-    </Progress>
-
-    <Progress strokeColor="#e0876a" :transitionDuration="1000" :radius="70" :strokeWidth="15" value="80.00">
-      <template v-slot:footer>
-        <b>General Electives</b>
-      </template>
-    </Progress>
-  --->
-
   </div>
 </template>
 
