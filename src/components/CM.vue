3ad6c45b59b6c9d549dbedbfbd86f05a641b2534
leowweisheng <leowweisheng@hotmail.com>
2020-03-20 16:57:21 +0800

    removed redundant codes and simplified module removal process

--- src/components/CM.vue
+++ src/components/CM.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="CM">
     <b>Core Modules</b><br> 
-    {{updateModules()}}
     <div v-if="Module">{{checkModule()}}</div>
-    <div v-for="(cm,index) in CompletedCore" :cm = "cm" :key="index">
+    <div v-for="(cm,index) in com_c" :cm = "cm" :key="index">
       {{cm}}   
       <button v-on:click="remove(cm)"> x </button>
     </div>
@@ -21,8 +20,7 @@ export default {
       core: ["BT1101", "CS1010S", "EC1301", "IS1103", "MA1101R", "MA1521", "MA1102R", "MKT1705X", "BT2101",
       "BT2102", "CS2030", "CS2040", "IS2101", "ST2334", "BT3102", "BT3103", "IS3103", "BT4103", "IS4010",
       "BT4101"],
-      lenC: 0,
-      CompletedCore: this.$store.getters.getModules
+      lenC: 0
     }
   },
   methods: {
@@ -30,25 +28,14 @@ export default {
       if(!this.com_c.includes(this.Module)) {
         if (this.core.includes(this.Module)) {
           this.$store.dispatch("addModule", this.Module);
-          this.lenC = this.com_c.length;
-          this.$emit('changeC', false);
+          this.$emit('changeC');
         }
       }
     },
+
     remove(x) {
-      this.$emit('removeC', x);
-      this.lenC = this.com_c.length;
-      this.$emit('changeC', this.lenC);
       this.$store.dispatch("removeModule", x);
-    },
-    updateModules() {
-      if (this.com_c.length == 0) {
-        if (this.CompletedCore.length != 0) {
-          this.com_c = this.CompletedCore;
-          this.lenC = this.com_c.length;
-          this.$emit('changeC', false);
-        }
-      }
+      this.$emit('changeC');
     }
   }
 }
